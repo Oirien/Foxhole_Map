7e4f2c8646af32c0e1ded839cf4d7036abab1c3b
Oirien <robert.mayo.ch@gmail.com>
2023-12-12 13:56:34 +0000

    potentially added db tracking

---
+++ api/queueGrab.js
@@ -0,0 +1,31 @@
+import { sql } from "@vercel/postgres";
+
+async function queueFunction() {
+    const queueResponse = await fetch("https://war-service-live.foxholeservices.com/external/shardStatus")
+        .then(res => res.json())
+        .then(data => data);
+
+    const pad = function (num) {
+        return ("00" + num).slice(-2);
+    };
+    let date;
+    date = new Date();
+    date =
+        date.getUTCFullYear() +
+        "-" +
+        pad(date.getUTCMonth() + 1) +
+        "-" +
+        pad(date.getUTCDate()) +
+        " " +
+        pad(date.getUTCHours()) +
+        ":" +
+        pad(date.getUTCMinutes()) +
+        ":" +
+        pad(date.getUTCSeconds());
+
+    const queueMap = await queueResponse.serverConnectionInfoList.map(grid => grid);
+    const totalWardenQueue = await queueMap.reduce((total, grid) => total + grid.wardenQueueSize, 0);
+    const totalColonialQueue = await queueMap.reduce((total, grid) => total + grid.colonialQueueSize, 0);
+
+    await sql`INSERT INTO queuelengths (Datetime, Colqueue, Warqueue) VALUES (${date}, ${totalColonialQueue}, ${totalWardenQueue})`;
+}
